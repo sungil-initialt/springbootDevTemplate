7e2eebeb52f89a1668b4a51b98bb7e15acc61e03
sungilry <sungilry@naver.com>
2024-06-10 15:31:39 +0900

    테스트

--- src/main/java/com/sptek/webfw/anotation/AnoCustomArgument.java
+++ src/main/java/com/sptek/webfw/anotation/AnoCustomArgument.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AnoCustomArgument {
-    //HandlerMethodArgumentResolver 를 적용할때 특정 어노테이션이 붙어 있는 경우만 적용하는 조건을 걸기 위해 만든 어노테이션임.
+    //HandlerMethodArgumentResolver 를 적용할때 특정 어노테이션이 붙어 있는 경우만 적용하는 조건을 걸기 위해 만든 어노테이션임
 }
